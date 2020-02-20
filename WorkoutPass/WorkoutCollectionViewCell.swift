ff7eb265d3467a2d39136b89992f342e9c993978
Nabi Jung <nabijung@icloud.com>
2020-02-20 14:59:50 -0500

    finished assignment

---
+++ WorkoutPass/WorkoutCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WorkoutCollectionViewCell.swift
+//  WorkoutPass
+//
+//  Created by nabi jung on 2/20/20.
+//
+
+import UIKit
+
+class WorkoutCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ WorkoutPass/WorkoutTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WorkoutTableViewCell.swift
+//  WorkoutPass
+//
+//  Created by nabi jung on 2/20/20.
+//
+
+import UIKit
+
+class WorkoutTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
